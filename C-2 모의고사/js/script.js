19825c674b025b936c9ccc4586dc155425249adb
eunkyo <jyk8929@naver.com>
2024-05-13 18:25:22 +0900

    C-2

--- C-2 모의고사/js/script.js
+++ C-2 모의고사/js/script.js
@@ -4,6 +4,26 @@ $(function(){
     }, function(){
         $(this).find(".submenu").stop().slideUp(200);
     });
+
+    let current = 0;
+
+    setInterval(function() {
+        
+        let next = (current+1)%3;
+
+        $(".slideList").find("div").eq(current).fadeOut();
+        $(".slideList").find("div").eq(next).fadeIn();
+    }, 3000);
+
+    $(".layerPopup").click(function(){
+        $(".layer").show();
+        $(".layer_bg").show();
+    });
+
+    $(".layer .close").click(function(){
+        $(".layer").hide();
+        $(".layer_bg").hide();
+    });
 });
 
 
