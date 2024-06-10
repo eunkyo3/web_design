2b5fa4f60bff74b8fa58562e65e389c8c31ae64a
JungEunKyo <jyk8929@naver.com>
2024-06-10 22:46:56 +0900

    C-4

--- C-4 모의고사/js/script.js
+++ C-4 모의고사/js/script.js
@@ -1,8 +1,16 @@
-$(function(){
-    setInterval(function(){
-        $('.slide ul').animate({left: -800}, 1000, function(){
-            $('.slide ul').eq(0).appendTo($('.slide ul'));
-            $('.slide ul').css({left: 0});
+$(function () {
+    setInterval(function () {
+        $('.slide ul').animate({ left: -800 }, 1000, function () {
+            $('.slide li').eq(0).appendTo($('.slide ul'));
+            $('.slide ul').css({ left: 0 });
         });
     }, 2000);
+
+    $('.notice li').eq(0).click(function () {
+        $('.modal').show();
+    });
+
+    $('.popup a').click(function () {
+        $('.modal').hide();
+    });
 });
