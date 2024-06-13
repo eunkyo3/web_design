3d60956b97a59ef3e9468ed75d2e69ef325d6256
eunkyo <jyk8929@naver.com>
2024-06-13 14:38:14 +0900

    A-3

--- A-3 모의고사/js/scipt.js
+++ A-3 모의고사/js/scipt.js
@@ -1,18 +1,32 @@
 $(function(){
-    $(".gnb>li").mouseover(function(){
-        $(this).find(".lnb").stop().slideDown();
-    });
+    let current = 0;
+    setInterval(function(){
+        let index = (current+1)%3;
+        $(".slide ul").find("li").eq(current).fadeOut();
+        $(".slide ul").find("li").eq(index).fadeIn();
+        current = index;
+    }, 2000);
 
-    // $("title_box").eq(1).click(function(){
-    //     $("title_box").eq(1).addClass(".active");
-    // });
-
+    $(".slide ul").animate(function(){
+        $(".slide li").eq(0).appendTo("slide ul");
+        $(".slide ul").css({left: -1200});
+    }, 2000);
 
     $(".title_box ul li").eq(1).click(function(){
-        $(".title_box ul li h2 a").eq(0).removeClass(".active");
-        $(".title_box ul li h2 a").eq(1).addClass(".active");
+        $(".title_box ul li h2 a").eq(0).removeClass("active");
+        $(".title_box ul li h2 a").eq(1).addClass("active");
+
+        $(".notice").hide();
+        $(".gallery img").show();
     });
 
+    $(".title_box ul li").eq(0).click(function(){
+        $(".title_box ul li h2 a").eq(1).removeClass("active");
+        $(".title_box ul li h2 a").eq(0).addClass("active");
+
+        $(".notice").show();
+        $(".gallery img").hide();
+    });
 
     $(".layerpopup").click(function(){
         $(".layer").show();
