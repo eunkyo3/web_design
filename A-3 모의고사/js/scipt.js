2f45f6f78fd9a54da89418a6d6d5cbaf5d2785d9
eunkyo <jyk8929@naver.com>
2024-06-13 09:38:25 +0900

    A-3

--- A-3 모의고사/js/scipt.js
+++ A-3 모의고사/js/scipt.js
@@ -1,9 +1,24 @@
-$(function () {
-    $(".gnb li").mosueover(function () {
-        $(".lnb li").stop().slideDown();
+$(function(){
+    $(".gnb>li").mouseover(function(){
+        $(this).find(".lnb").stop().slideDown();
     });
 
-    $("title_box").eq(1).click(function () {
-        $("title_box").eq(1).addClass(".active");
+    // $("title_box").eq(1).click(function(){
+    //     $("title_box").eq(1).addClass(".active");
+    // });
+
+
+    $(".title_box ul li").eq(1).click(function(){
+        $(".title_box ul li h2 a").eq(0).removeClass(".active");
+        $(".title_box ul li h2 a").eq(1).addClass(".active");
     });
+
+
+    $(".layerpopup").click(function(){
+        $(".layer").show();
+
+        $(".layer a").click(function(){
+            $(".layer").hide();
+        });
+    })
 });
