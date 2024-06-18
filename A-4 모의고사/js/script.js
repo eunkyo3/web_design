c75a6e313f1e74adf265d77e73968a4b890d77d9
eunkyo <jyk8929@naver.com>
2024-06-18 16:39:04 +0900

    js 한줄

--- A-4 모의고사/js/script.js
+++ A-4 모의고사/js/script.js
@@ -1,12 +1,17 @@
 $(function(){
 
-    let current = 0;
-    setInterval(function() {
-        let next = (current + 1) % 3;
-        $(".slide li").eq(current).fadeOut(1000);
-        $(".slide li").eq(next).fadeIn(1000);
-        current = next;
-    }, 2000);
+    // let current = 0;
+    // setInterval(function() {
+    //     let next = (current + 1) % 3;
+    //     $(".slide li").eq(current).fadeOut(1000);
+    //     $(".slide li").eq(next).fadeIn(1000);
+    //     current = next;
+    // }, 2000);
+
+    $(".slide ul li:gt(0)").hide();
+        setInterval(function() {
+        $(".slide ul li:first").fadeOut().next().fadeIn().end().appendTo(".slide ul"); 
+    }, 1000);
 
     $(".gnb li a").mouseover(function(){
         $(".bg").show();
